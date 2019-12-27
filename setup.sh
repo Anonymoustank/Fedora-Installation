a824edca3f4d9b2b32230ec05e7fd27ceae6d9bf
Anonymoustank <54193508+Anonymoustank@users.noreply.github.com>
2019-12-26 21:00:31 -0700

    Removed traces of sudo

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-sudo dnf -y install passwd
+dnf -y install passwd
 
-sudo dnf -y install cracklib-dicts
+dnf -y install cracklib-dicts
 
-sudo dnf -y install findutils
+dnf -y install findutils
 
 exec 0<&1
 
@@ -12,6 +12,6 @@ read -p 'Enter your chroot username: ' new_user
 
 useradd $new_user
 
-sudo usermod -aG wheel $new_user
+usermod -aG wheel $new_user
 
 passwd $new_user
